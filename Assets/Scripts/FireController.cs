595825ba70fb3d481d6511ced380c61e4d0a477a
marty-p <37085112+marty-p@users.noreply.github.com>
2019-01-08 15:13:00 +0000

    Update FireController.cs

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -2,10 +2,11 @@ using Unity;
 public class FireController : MonoBehavior {
     private ParticleSystem ps;
     private float lifetime = 15.0f;
-  
+
     void Start() {
         ps = GetComponent<ParticleSystem>();
         var module = ps.shape;
+        module.enabled = true;
         module.shapeType = ParticleSystemShapeType.Circle;
     }
 
